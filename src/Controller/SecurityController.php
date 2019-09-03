8a9101d2bb2bafb5885287a2cb95ee8f7651b825
Aurelien Walter <aurelienwalter@orange.fr>
2019-09-03 17:13:45 +0200

    Login Inscription Logout

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -14,6 +14,9 @@ class SecurityController extends AbstractController
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
+        if ($this->isGranted('ROLE_USER')) {
+            return $this->redirectToRoute('home');
+        }
         // if ($this->getUser()) {
         //    $this->redirectToRoute('target_path');
         // }

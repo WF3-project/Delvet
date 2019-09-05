502be9f9b8baac1bd534df06df83f4afa6045081
Tahxyonn <anthony.catelle@gmail.com>
2019-09-05 16:20:49 +0200

    fix update

--- src/Controller/ChatController.php
+++ src/Controller/ChatController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Messages;
+use App\Form\MessagesType;
 
 class ChatController extends AbstractController
 {

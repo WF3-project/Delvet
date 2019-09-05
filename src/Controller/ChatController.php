da821373578776b3a925b4acff80daf381b47c13
Tahxyonn <anthony.catelle@gmail.com>
2019-09-05 12:00:20 +0200

    comment

--- src/Controller/ChatController.php
+++ src/Controller/ChatController.php
@@ -16,4 +16,11 @@ class ChatController extends AbstractController
             'controller_name' => 'ChatController',
         ]);
     }
+    //1. Création d'une fonction pour générer la route depuis un twig ( a peu près = include)
+
+    //2. Création d'une fonction pour load les messages depuis la BDD
+
+    //3. Création d'une fonction pour l'envoi des messages 
+
 }
+

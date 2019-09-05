e0a585184d5fc5e16d3bb8a19df8b55d6ef97064
Tahxyonn <anthony.catelle@gmail.com>
2019-09-05 12:44:56 +0200

    création du squelette

--- src/Controller/ChatController.php
+++ src/Controller/ChatController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\MessagesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,17 +11,29 @@ class ChatController extends AbstractController
     /**
      * @Route("/chat", name="chat")
      */
-    public function index()
+    public function index(MessagesRepository $messagesRepository)
     {
         return $this->render('chat/index.html.twig', [
             'controller_name' => 'ChatController',
+            'messages' => $messagesRepository->findAll()
         ]);
     }
     //1. Création d'une fonction pour générer la route depuis un twig ( a peu près = include)
-
+  
     //2. Création d'une fonction pour load les messages depuis la BDD
+    public function getMessages()
+    {
 
+    }
     //3. Création d'une fonction pour l'envoi des messages 
+    public function sendMessages()
+    {
+        $messages= new Messages();
 
+        if ($messages->isSubmitted())
+        {
+        $this->getDoctrine()->getManager()->flush();
+        }
+    }
 }
 

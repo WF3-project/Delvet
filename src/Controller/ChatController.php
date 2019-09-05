365925de3796e78a4a6a69457d673480175a4ca4
Cedric <ced.spychala@gmail.com>
2019-09-05 15:56:28 +0200

    fix chat

--- src/Controller/ChatController.php
+++ src/Controller/ChatController.php
@@ -28,22 +28,19 @@ class ChatController extends AbstractController
         'messages' => $messagesRepository->findAll()
         ]);
     }
-    //3. Création d'une fonction pour l'envoi des messages 
-    /**
-     * @Route("/chat", name="chat")
-     */
-    public function sendMessages(Request $request)
-    {
-        $message= new Messages();
-        $form = $this->createForm(MessagesType::class, $message);
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid())
-        {
-            // On récupere Doctrine pour gérer la BDD
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($message);
-            $entityManager->flush();
-        }
-    }
+
+    // public function sendMessages(Request $request)
+    // {
+    //     $message= new Messages();
+    //     $form = $this->createForm(MessagesType::class, $message);
+    //     $form->handleRequest($request);
+    //     if ($form->isSubmitted() && $form->isValid())
+    //     {
+    //         // On récupere Doctrine pour gérer la BDD
+    //         $entityManager = $this->getDoctrine()->getManager();
+    //         $entityManager->persist($message);
+    //         $entityManager->flush();
+    //     }
+    // }
 }
 

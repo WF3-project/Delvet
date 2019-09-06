23b5e5114a98f5037702b3f61ad6eeaccb2f2179
Tahxyonn <anthony.catelle@gmail.com>
2019-09-06 11:04:00 +0200

    fix unmerged

--- config/bundles.php
+++ config/bundles.php
@@ -15,10 +15,6 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
-<<<<<<< HEAD
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
-=======
-    FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all'=> true],
->>>>>>> develop
 ];

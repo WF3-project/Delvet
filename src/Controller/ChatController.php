7f7f348ff30005c51d0ddbceafd6b2163f4809a2
Tahxyonn <anthony.catelle@gmail.com>
2019-09-06 13:03:24 +0200

    ajout de node.js pour utiliser socket.io

--- src/Controller/ChatController.php
+++ src/Controller/ChatController.php
@@ -2,12 +2,8 @@
 
 namespace App\Controller;
 
-use App\Repository\MessagesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Request;
-use App\Entity\Messages;
-use App\Form\MessagesType;
 
 class ChatController extends AbstractController
 {
@@ -20,44 +16,4 @@ class ChatController extends AbstractController
             'controller_name' => 'ChatController',
         ]);
     }
-    //1. Création d'une fonction pour générer la route depuis un twig ( a peu près = include)
-  
-    //2. Création d'une fonction pour load les messages depuis la BDD
-    public function getMessages(MessagesRepository $messagesRepository)
-    {
-        return $this->render('chat/index.html.twig', [
-        'messages' => $messagesRepository->findAll()
-        ]);
-    }
-    //3. Création d'une fonction pour l'envoi des messages 
-    /**
-     * @Route("/chat_update", name="chat_update")
-     */
-    public function sendMessages(Request $request)
-    {
-        if(!array_key_exists('author', $_POST) || !array_key_exists('content', $_POST)){
-
-            echo json_encode(["status" => "error", "message" => "One field or many have not been sent"]);
-            return;
-        
-        }
-        $message= new Messages();
-        $form = $this->createForm(MessagesType::class, $message);
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid())
-        {
-            // On récupere Doctrine pour gérer la BDD
-            echo json_encode(["status" => "success"]);
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($message);
-            $entityManager->flush();
-           
-            return $this->redirectToRoute('home');
-           
-        }
-        return $this->render('home/index.html.twig', [
-            'message' => $message,
-        ]);
-    }
 }
-

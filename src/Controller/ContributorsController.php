f0ba828d84a7f2641499d5f91ae8b2cdbf6a3851
Cedric Spychala <cedric@MacBook-Air-de-Cedric.local>
2019-09-11 14:35:21 +0200

    contributors

--- src/Controller/ContributorsController.php
+++ src/Controller/ContributorsController.php
@@ -4,16 +4,22 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Contributors;
+use App\Repository\ContributorsRepository;
+use Symfony\Component\HttpFoundation\Response;
 
 class ContributorsController extends AbstractController
-{
+{   
+
+    
     /**
      * @Route("/contributors", name="contributors")
      */
-    public function index()
+    public function index(contributorsRepository $contributorsRepository)
     {
         return $this->render('contributors/index.html.twig', [
-            'controller_name' => 'ContributorsController',
+            'controller_name' => $contributorsRepository->findAll(),
+            dump($contributorsRepository->findAll()),
         ]);
     }
 }

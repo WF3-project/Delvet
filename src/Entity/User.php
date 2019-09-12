76f9a48660740c48876cfb650aa2beeff9967c74
cedric <ced.spychala@gmail.com>
2019-09-12 17:37:16 +0200

    migration

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -35,7 +35,7 @@ class User implements UserInterface
     private $email;
 
     /**
-     * @ORM\Column(type="json")
+     * @ORM\Column(type="array")
      */
     private $roles = [];
 
--- src/Migrations/Version20190912153256.php
+++ src/Migrations/Version20190912153256.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190912093122 extends AbstractMigration
+final class Version20190912153256 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,10 +22,9 @@ final class Version20190912093122 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE contributors CHANGE user_id user_id INT NOT NULL');
         $this->addSql('ALTER TABLE courses CHANGE categories_id categories_id INT DEFAULT NULL, CHANGE contributors_id contributors_id INT DEFAULT NULL');
-        $this->addSql('ALTER TABLE messages CHANGE content content LONGTEXT NOT NULL');
-        $this->addSql('ALTER TABLE user CHANGE roles roles JSON NOT NULL, CHANGE reset_token reset_token VARCHAR(255) DEFAULT NULL, CHANGE confirmation_token confirmation_token TINYINT(1) DEFAULT NULL, CHANGE enabled enabled TINYINT(1) DEFAULT NULL');
+        $this->addSql('ALTER TABLE user CHANGE roles roles LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\', CHANGE reset_token reset_token VARCHAR(255) DEFAULT NULL, CHANGE enabled enabled TINYINT(1) DEFAULT NULL, CHANGE confirmation_token confirmation_token VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE contributors CHANGE user_id user_id INT NOT NULL');
     }
 
     public function down(Schema $schema) : void
@@ -35,7 +34,6 @@ final class Version20190912093122 extends AbstractMigration
 
         $this->addSql('ALTER TABLE contributors CHANGE user_id user_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE courses CHANGE categories_id categories_id INT DEFAULT NULL, CHANGE contributors_id contributors_id INT DEFAULT NULL');
-        $this->addSql('ALTER TABLE messages CHANGE content content LONGTEXT DEFAULT NULL COLLATE utf8mb4_unicode_ci');
         $this->addSql('ALTER TABLE user CHANGE roles roles LONGTEXT NOT NULL COLLATE utf8mb4_bin, CHANGE reset_token reset_token VARCHAR(255) DEFAULT \'NULL\' COLLATE utf8mb4_unicode_ci, CHANGE enabled enabled TINYINT(1) DEFAULT \'NULL\', CHANGE confirmation_token confirmation_token TINYINT(1) DEFAULT \'NULL\'');
     }
 }

b0aa9457d42ab3c54d4cae5b8aeb6a2478369624
bichuci <aurelienwalter@orange.fr>
2019-09-12 13:36:51 +0200

    responsive

---
+++ src/Controller/ContributorsController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Contributors;
+use App\Repository\ContributorsRepository;
+use Symfony\Component\HttpFoundation\Response;
+
+class ContributorsController extends AbstractController
+{   
+
+    
+    /**
+     * @Route("/contributors", name="contributors")
+     */
+    public function index(ContributorsRepository $contributorsRepository)
+    {
+       
+       
+        return $this->render('contributors/index.html.twig', [
+            'contributors' => $contributorsRepository->findAll(),
+           
+        ]);
+    }
+
+    /**
+     * @Route("/contributors/course/{id}", name="contributorsCourse")
+     */
+    public function contributorCourse( Contributors $contributor ,ContributorsRepository $contributorsRepository)
+    {      
+        return $this->render('contributors/contributorCourse.html.twig', [
+            'courses' => $contributor->getCourseCreate(),
+            'contributor'=> $contributor
+        ]);
+    } 
+    
+}
